8b21b88f4332f2e6300f27ff32923f1d1013e49d
almiraazr <almira.hufaida@gmail.com>
2024-10-23 11:57:32 +0700

    update tailwind

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,23 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login Page</title>
-    <link rel="stylesheet" href="login.css">
-  </head>
-  <body>
-    <form id="loginForm">
-      <div>
-        <label for="username">Username: </label>
-        <input type="text" name="username" id="username" />
+<html>
+<head>
+  <title>Login Page</title>
+  @vite('resources/css/app.css')
+  <script src="https://registry.npmmirror.com/vue/3.3.11/files/dist/vue.global.js"></script>
+</head>
+<body class="bg-gray-100 flex items-center justify-center min-h-screen">
+  <div id="app" class="w-full max-w-xs">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+          Username:
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
       </div>
-      <div>
-        <label for="password">Password: </label>
-        <input type="password" name="password" id="password" />
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+          Password:
+        </label>
+        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password">
       </div>
-      <button type="submit">Submit</button>
+      <div class="flex items-center justify-between">
+        <button class="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+          Submit
+        </button>
+      </div>
     </form>
-    <script src="js/script.js"></script>
-  </body>
+  </div>
+  <script>
+    const { createApp } = Vue
+    createApp({}).mount('#app')
+  </script>
+</body>
 </html>
